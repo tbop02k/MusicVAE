722ba195cd8d9380ef8b9edcfb155f5d920420d8
Jin <tbop02@kaist.ac.kr>
2021-11-16 23:44:28 +0900

    FIRST COMMIT
    Makes dataloader (for pytorch traning and validating) as spliting data into train and valid set.

---
+++ dataloader.py
@@ -0,0 +1,33 @@
+import pickle
+import numpy as np
+from torch.utils.data import DataLoader, random_split, Dataset
+
+import config
+
+class MidiDataSet(Dataset):
+    def __init__(self, x):
+        self.x = x
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, idx):
+        return self.x[idx].astype('float32')
+    
+
+def main():
+
+    with open(config.path_data_pickle, 'rb') as f:
+        data = pickle.load(f)    
+    
+    train_size = int(config.train_ratio* len(data))
+    valid_size = int(len(data) - train_size)
+    batch_size = config.train_batch_size
+
+    trainset, validset = random_split(data, [train_size, valid_size])
+
+    train_loader = DataLoader(MidiDataSet(trainset), shuffle=True, batch_size=batch_size, num_workers=4)
+    valid_loader = DataLoader(MidiDataSet(validset), shuffle=True, batch_size=batch_size, num_workers=4)
+
+
+    return train_loader, valid_loader
